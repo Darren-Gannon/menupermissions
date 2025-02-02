0c181a9f6ec9d9ad25ae9ba4565991869872faa8
darren <darrengannon93@gmail.com>
2025-02-02 14:27:36 +0200

    test: add pressure tests for merging large user and permission datasets

--- src/merge/merge.spec.ts
+++ src/merge/merge.spec.ts
@@ -16,6 +16,35 @@ describe('Merge', () => {
         });
     });
 
+    describe('Pressure Tests', () => {
+        it('should merge a 10000 users with 200 permissions', () => {
+            const NUM_USERS = 10000;
+            const NUM_MENUS = 200;
+            const users = [
+                ...new Array(NUM_USERS),
+            ].map((_, i) => ({ userName: `user${i}`, menuItems: [true, ...new Array(NUM_MENUS - 1).fill(false)] }));
+            const menus = [
+                ...new Array(NUM_MENUS),
+            ].map((_, i) => `menu${i}`);
+            const result = merge(users, menus, {});
+            expect(result).toBeDefined();
+        });
+
+        it('should merge a large list of users with a large list of permissions', () => {
+            const NUM_USERS = 100000;
+            const NUM_MENUS = 4000;
+            const users = [
+                ...new Array(NUM_USERS),
+            ].map((_, i) => ({ userName: `user${i}`, menuItems: [true, ...new Array(NUM_MENUS - 1).fill(false)] }));
+            const menus = [
+                ...new Array(NUM_MENUS),
+            ].map((_, i) => `menu${i}`);
+            const result = merge(users, menus, {});
+            expect(result).toBeDefined();
+        });
+            
+    });
+
     it('should throw an error due to an insuffienct number of permissions', () => {
         const users = [
             { userName: 'user1', menuItems: [true, false] },
