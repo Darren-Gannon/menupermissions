41ac97f603d8d1b51360013e15866c93d2e951c3
darren <darrengannon93@gmail.com>
2025-02-02 12:18:12 +0200

    chore: add test for parsing menus with custom line delimiter

--- src/menu-parser/menu.parser.spec.ts
+++ src/menu-parser/menu.parser.spec.ts
@@ -1,3 +1,4 @@
+import { EOL } from "node:os";
 import { parseMenusStr } from "./menu.parser";
 
 describe('MenuParser', () => {
@@ -149,5 +150,31 @@ describe('MenuParser', () => {
                 Help Menu
             `, { lineDelimiter: '\n', strictIndexing: true });
         }).toThrow('Invalid menu item on line 1. Expected an index');
+    });
+
+    it('should parse a menu with a custom line delimiter', () => {
+        expect(parseMenusStr([
+            "1, Applications Menu",
+            "2, Security Permissions Menu",
+            "3, Customers Menu",
+            "4, Accounts Menu",
+            "6, Settings Menu",
+            "5, Pricing Menu",
+            "7, Orders Menu",
+            "8, Jobs Menu",
+            "9, Profile Menu",
+            "10, Help Menu",
+        ].join(EOL))).toEqual([
+            'Applications Menu',
+            'Security Permissions Menu',
+            'Customers Menu',
+            'Accounts Menu',
+            'Pricing Menu',
+            'Settings Menu',
+            'Orders Menu',
+            'Jobs Menu',
+            'Profile Menu',
+            'Help Menu',
+        ]);
     });
 });
