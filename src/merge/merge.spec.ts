cb571f57298f70a9f9d8c41a1ddd80525c5e9f0c
darren <darrengannon93@gmail.com>
2025-02-02 12:37:01 +0200

    feat: add error handling tests for insufficient permissions and empty menus in merge function

--- src/merge/merge.spec.ts
+++ src/merge/merge.spec.ts
@@ -15,4 +15,34 @@ describe('Merge', () => {
             ],
         });
     });
+
+    it('should throw an error due to an insuffienct number of permissions', () => {
+        const users = [
+            { userName: 'user1', menuItems: [true, false] },
+        ];
+        const menus = ['menu1', 'menu2', 'menu3'];
+        expect(() => {
+            merge(users, menus, { ensureMenuItems: true })
+        }).toThrow('User "user1" has not been defined with sufficent permissions. Expected 3 but got 2');
+    });
+
+    it('should throw an error due to an insuffienct number of menus', () => {
+        const users = [
+            { userName: 'user1', menuItems: [true, false] },
+        ];
+        const menus = ['menu1'];
+        expect(() => {
+            merge(users, menus, { ensureMenuItems: true })
+        }).toThrow('User [0] "user1" has more permissions than there are menu items');
+    });
+
+    it('should throw an error due to an empty menu', () => {
+        const users = [
+            { userName: 'user1', menuItems: [true] },
+        ];
+        const menus = [''];
+        expect(() => {
+            merge(users, menus, { ensureMenuItems: true })
+        }).toThrow('Empty menu item found on menu item 1');
+    });
 });
